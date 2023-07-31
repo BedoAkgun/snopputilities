1a47a84250e7f2c953f4ea35334d5dfc09b1dc5c
GabrielX24 <Gabriele@DESKTOP-0FC9JLK>
2023-07-31 19:31:25 +0200

    Staffs and power potions

---
+++ src/main/java/net/bedirakgun/snoppsadditions/procedures/EPowerRightClickedProcedure.java
@@ -0,0 +1,67 @@
+package net.bedirakgun.snoppsadditions.procedures;
+
+import net.minecraft.world.GameType;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.Minecraft;
+
+import net.bedirakgun.snoppsadditions.SnoppsAdditionsMod;
+
+import java.util.Random;
+import java.util.Map;
+
+public class EPowerRightClickedProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				SnoppsAdditionsMod.LOGGER.warn("Failed to load dependency entity for procedure EPowerRightClicked!");
+			return;
+		}
+		if (dependencies.get("itemstack") == null) {
+			if (!dependencies.containsKey("itemstack"))
+				SnoppsAdditionsMod.LOGGER.warn("Failed to load dependency itemstack for procedure EPowerRightClicked!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.STRENGTH, (int) 300, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 20, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.MINING_FATIGUE, (int) 20, (int) 3));
+		if ((new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.CREATIVE;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((AbstractClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.CREATIVE;
+				}
+				return false;
+			}
+		}.checkGamemode(entity)) == false) {
+			{
+				ItemStack _ist = itemstack;
+				if (_ist.attemptDamageItem((int) 1, new Random(), null)) {
+					_ist.shrink(1);
+					_ist.setDamage(0);
+				}
+			}
+			if (entity instanceof PlayerEntity)
+				((PlayerEntity) entity).getCooldownTracker().setCooldown(itemstack.getItem(), (int) 1000);
+		} else {
+			if (entity instanceof PlayerEntity)
+				((PlayerEntity) entity).getCooldownTracker().setCooldown(itemstack.getItem(), (int) 20);
+		}
+	}
+}
